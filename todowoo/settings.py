d7c235dc1cccc768c07ab0cda6bf8918d92f5de0
FedorKros <klimovichf@gmail.com>
2021-08-16 21:33:10 +0300

    Local settings

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -132,4 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
 
-
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
